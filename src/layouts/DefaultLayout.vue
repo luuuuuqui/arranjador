18977a88ecb4325589e023bcfc6d3bec18b81c63
Lucas Duarte <196474158+luuuuuqui@users.noreply.github.com>
2026-02-08 03:00:04 -0300

    feat(DefaultLayout): add new layout component with header and main content area

---
+++ src/layouts/DefaultLayout.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="default-layout">
+    <HeaderSmall />
+    <main>
+      <router-view />
+    </main>
+  </div>
+</template>
+
+<script setup>
+import HeaderSmall from '@/components/HeaderSmall.vue'
+</script>
