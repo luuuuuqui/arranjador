d1a9538f082b20cf2a742c201418686afe680c42
Lucas Duarte <196474158+luuuuuqui@users.noreply.github.com>
2026-04-21 21:26:53 -0300

    fix: update build script to copy index.html as 404.html and adjust router history mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHashHistory } from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue"
 
 const routes = [
@@ -61,9 +61,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
-  history: createWebHashHistory(),
 });
 
 router.beforeEach((to, from, next) => {
